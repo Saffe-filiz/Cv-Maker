11bfeb2bf8737e8e855cee6f1f3fc40a5c35398e
Saffe-filiz <saffetfiliz1989@gmail.com>
2023-06-30 20:58:06 +0300

    Top bar İnput style

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -1,14 +1,17 @@
 <template>
-	<div class="hidden xl:flex w-full h-[70px]  px-6 justify-between items-center">
-		<div class="w-auto h-[47px] bg-red-100 flex items-center">
+	<div class="hidden xl:flex w-full h-[70px] bg-white px-6 justify-between items-center">
+		<div class="w-auto h-[47px]  flex items-center">
 			<button class="w-auto h-[45px] px-6 flex items-center justify-center bg-[#4372ff] rounded">
 				<span class="mr-2.5 w-6 h-6">
 					<img class="w-full h-full" src="../assets/grid.svg">
 				</span>
-				<p>CV models</p>
+				<p class="text-white">CV models</p>
 			</button>
-			<div class="w-[248px] h-[45px] flex items-center justify-center">
-				<input type="text" name="cv name">
+			<div class="w-[248px] h-[45px] flex items-center justify-between border border-[#0000001f] rounded mx-4 focus-within:border-[#4372FF] hover:border-black">
+				<input class="w-full h-full bg-white outline-0 pl-3.5 rounded" type="text" name="cv name">
+				<span class="w-[21.3px] h-[21.3px] mr-3.5">
+					<img class="w-full h-full" src="../assets/pen.svg">
+				</span>
 			</div>
 			<button class="w-[70px] h-full flex items-center justify-center  border border-[#4372FF] rounded">
 				<span class="w-5 h-5">
