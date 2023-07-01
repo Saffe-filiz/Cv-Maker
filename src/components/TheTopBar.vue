9ae68739713e96ceb43aaec16e10412cedd10a90
Saffe-filiz <saffetfiliz1989@gmail.com>
2023-07-01 16:45:15 +0300

    Dowland Button

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -7,7 +7,7 @@
 				</span>
 				<p class="text-white">CV models</p>
 			</button>
-			<div class="w-[248px] h-[45px] flex items-center justify-between border border-[#0000001f] rounded mx-4 focus-within:border-[#4372FF] hover:border-black">
+			<div class="w-[248px] h-[45px] flex items-center justify-between border border-[#0000001f] rounded mx-4 focus-within:border-[#4372FF] hover:border-black hover:focus-within:border-[#4372FF]">
 				<input class="w-full h-full bg-white outline-0 pl-3.5 rounded" type="text" name="cv name">
 				<span class="w-[21.3px] h-[21.3px] mr-3.5">
 					<img class="w-full h-full" src="../assets/pen.svg">
@@ -19,7 +19,22 @@
 				</span>
 			</button>
 		</div>
-		<div class="w-[653px] h-[47px] bg-red-100"></div>
+		<div class="w-[653px] h-[47px] bg-red-100 flex gap-x-[16px]">
+			<div class="w-[134px] h-full bg-orange-100 rounded flex items-center">
+				<span class="w-9 h-full flex items-center justify-center bg-[#f5f5f5]">
+					<img class="w-5 h-7" src="../assets/colors.svg">
+				</span>
+				<div class="w-full h-full bg-white">
+					<div class="w-12 h-7 bg-black border "></div>
+				</div>
+			</div>
+			<button class="w-[214px] h-full rounded bg-[#50af81] flex items-center justify-center">
+			    <span class="w-6 h-6 mr-4">
+				    <img class="w-full h-full" src="../assets/dowlandCv.svg">
+			    </span>
+			    <span class="text-white">DOWNLOAD MY CV</span>
+		   </button>
+		</div>
 	</div>
 </template>
 
