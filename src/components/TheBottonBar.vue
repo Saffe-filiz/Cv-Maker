aa9b9382c72c28de9ca3457f5d58e34ebbb794c6
Saffe-filiz <saffetfiliz1989@gmail.com>
2023-07-04 02:06:50 +0300

    Button Bar

--- src/components/TheBottonBar.vue
+++ src/components/TheBottonBar.vue
@@ -1,3 +1,18 @@
 <template>
-	<div  class="w-full h-[64px] inline xl:hidden bg-red-300">Bottom Bar</div>
+	<div  class="w-full h-16 inline xl:hidden absolute -bottom-16 left-0 flex justify-center border-t border-t-borderColor bg-white">
+		<div class="max-w-[492px] w-full h-full  inline-flex mx-auto">
+				<button class="w-full h-full flex flex-col justify-center items-center">
+				<span class="w-full h-6 flex justify-center"><svg  viewBox="0 0 24 24" fill="#00000099"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-5 14H4v-4h11v4zm0-5H4V9h11v4zm5 5h-4V9h4v9z"></path></svg></span>
+				<span class="w-full h-auto text-center text-xs">CV templates</span>
+			</button>
+			<button class="w-full block max-w-[168px] max-w-[168px] h-full flex flex-col justify-center border-x border-x-borderColor">
+				<span class="w-full h-6 flex justify-center"><svg viewBox="0 0 24 24" fill="#00000099"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg></span>
+				<span class="w-full h-auto text-center text-xs">Editor</span>
+			</button>
+				<button class="w-full h-full flex flex-col justify-center">
+				<span class="w-full h-6 flex justify-center"><svg viewBox="0 0 24 24" fill="#00000099"><path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm0 16H5V7h14v12zm-5.5-6c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM12 9c-2.73 0-5.06 1.66-6 4 .94 2.34 3.27 4 6 4s5.06-1.66 6-4c-.94-2.34-3.27-4-6-4zm0 6.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"></path></svg></span>
+				<span class="w-full h-auto text-center">Prevewe</span>
+			</button>
+		</div>
+	</div>
 </template>
--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="w-full h-full  relative overflow-y-auto">
-		<div class="w-full bg-white absolute bottom-0 left-0 p-4 xl:p-8 border-t border-borderColor">
-			<div class="w-full min-w-[485px] flex justify-between">
+		<div class="w-full bg-white absolute bottom-16 xl:bottom-0 left-0 p-4 xl:p-8 border-t border-borderColor">
+			<div class="w-full  flex justify-between">
 				<button class="h-[38px] px-6 text-[#4372ff] bg-white border border-[#4372ff] rounded">GO BACK</button>
 				<button class="h-[38px] px-6 text-white bg-[#4372ff] border border-[#4372ff] rounded shadow-[0px_4px_7px_0px_rgba(67,114,255,0.3)]">NEXT</button>
 			</div>
-			   <BottonBar/>
+			  <BottonBar/>
 		</div>
 	</div>
 </template>
