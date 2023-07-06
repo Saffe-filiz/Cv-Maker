debfd6ae6c682ade3c19f6a7e061fda819a0a2d5
Saffe-filiz <saffetfiliz1989@gmail.com>
2023-07-06 22:19:07 +0300

    Scroll Bart Icon fuction

--- src/components/TheSideBar.vue
+++ src/components/TheSideBar.vue
@@ -1,15 +1,15 @@
 <template>
-	<aside class="hidden xl:block w-[111px] h-full bg-[#9fb1db]" >
+	<aside class="hidden xl:block w-[111px] h-screen bg-[#9fb1db]" ref="container">
 		<div class="h-[70px] w-full"></div>
-		<div class="w-full h-full flex flex-col">
-			<button class="w-full h-10 bg-[#9fb1db] flex-none" v-show="currentHeight">
-			    <svg class="w-6 h-6 mx-auto" fill="#0000008a" viewBox="0 0 24 24">
-				    <path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
-			    </svg>
+		<div class="w-full max-h-[80vh] flex flex-col">
+			<button class="w-full h-10 bg-[#9fb1db] flex-none" v-show="currentHeight" >
+			    	<svg class="w-6 h-6 mx-auto" :class="{'invisible': status.firstPosition}" fill="#0000008a" viewBox="0 0 24 24">
+			    		<path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
+			    	</svg>
 		    </button>
-			<div class="w-full h-auto overflow-y-auto overflow-x-hidden sideNav">
+			<div class="w-full h-auto overflow-y-auto overflow-x-hidden sideNav" ref="sideList">
 			    <ul class="w-full  flex flex-col items-center gap-y-[5px]">
-				    <li class="w-[94px] h-20 rounded flex justify-center flex-col items-center group cursor-pointer" :class="{'bg-[#e7ecf9]': true}">
+				    <li class="w-[94px] h-20 rounded flex justify-center flex-col items-center group cursor-pointer" :class="{'bg-[#e7ecf9]': true}" ref="item">
 					<span >
 						<svg class="group-hover:fill-[#4372FF]" :class="{'fill-[#4372FF]': true}" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" ><path d="M22 12C22 6.49 17.51 2 12 2C6.49 2 2 6.49 2 12C2 14.9 3.25 17.51 5.23 19.34C5.23 19.35 5.23 19.35 5.22 19.36C5.32 19.46 5.44 19.54 5.54 19.63C5.6 19.68 5.65 19.73 5.71 19.77C5.89 19.92 6.09 20.06 6.28 20.2C6.35 20.25 6.41 20.29 6.48 20.34C6.67 20.47 6.87 20.59 7.08 20.7C7.15 20.74 7.23 20.79 7.3 20.83C7.5 20.94 7.71 21.04 7.93 21.13C8.01 21.17 8.09 21.21 8.17 21.24C8.39 21.33 8.61 21.41 8.83 21.48C8.91 21.51 8.99 21.54 9.07 21.56C9.31 21.63 9.55 21.69 9.79 21.75C9.86 21.77 9.93 21.79 10.01 21.8C10.29 21.86 10.57 21.9 10.86 21.93C10.9 21.93 10.94 21.94 10.98 21.95C11.32 21.98 11.66 22 12 22C12.34 22 12.68 21.98 13.01 21.95C13.05 21.95 13.09 21.94 13.13 21.93C13.42 21.9 13.7 21.86 13.98 21.8C14.05 21.79 14.12 21.76 14.2 21.75C14.44 21.69 14.69 21.64 14.92 21.56C15 21.53 15.08 21.5 15.16 21.48C15.38 21.4 15.61 21.33 15.82 21.24C15.9 21.21 15.98 21.17 16.06 21.13C16.27 21.04 16.48 20.94 16.69 20.83C16.77 20.79 16.84 20.74 16.91 20.7C17.11 20.58 17.31 20.47 17.51 20.34C17.58 20.3 17.64 20.25 17.71 20.2C17.91 20.06 18.1 19.92 18.28 19.77C18.34 19.72 18.39 19.67 18.45 19.63C18.56 19.54 18.67 19.45 18.77 19.36C18.77 19.35 18.77 19.35 18.76 19.34C20.75 17.51 22 14.9 22 12ZM16.94 16.97C14.23 15.15 9.79 15.15 7.06 16.97C6.62 17.26 6.26 17.6 5.96 17.97C4.44 16.43 3.5 14.32 3.5 12C3.5 7.31 7.31 3.5 12 3.5C16.69 3.5 20.5 7.31 20.5 12C20.5 14.32 19.56 16.43 18.04 17.97C17.75 17.6 17.38 17.26 16.94 16.97Z"></path><path d="M12 6.93005C9.93 6.93005 8.25 8.61005 8.25 10.6801C8.25 12.7101 9.84 14.3601 11.95 14.4201C11.98 14.4201 12.02 14.4201 12.04 14.4201C12.06 14.4201 12.09 14.4201 12.11 14.4201C12.12 14.4201 12.13 14.4201 12.13 14.4201C14.15 14.3501 15.74 12.7101 15.75 10.6801C15.75 8.61005 14.07 6.93005 12 6.93005Z"></path></svg>
 					</span>
@@ -54,8 +54,8 @@
 		        </ul>
 			</div> 
 			<button class="w-full h-10 bg-[#9fb1db]" v-show="currentHeight">
-			    <svg class="w-6 h-6 mx-auto transform rotate-180" fill="#0000008a" viewBox="0 0 24 24">
-				    <path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
+			    <svg class="w-6 h-6 mx-auto transform rotate-180" :class="{'invisible': status.secondPostion}" fill="#0000008a" viewBox="0 0 24 24">
+			    	<path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
 			    </svg>
 		    </button>
 		</div>
@@ -63,18 +63,30 @@
 </template>
 
 <script setup>
-	import { ref, computed, onMounted, watch, watchEffect } from 'vue';
-	let contentHeight = ref(0);
-	let currentHeight = ref(null);
+	import { ref, reactive, onMounted } from 'vue';
 
-	const  currentIndex = ref(0);
+	let currentIndex = ref(0);
+	let sideList = ref(null);
+	let item = ref(null);
+	let status = reactive({
+		firstPosition: null,
+		secondPostion: null,
+	})
 
-	onMounted(() => window.addEventListener("resize", pageResize))
+	const container = ref(null);
 
-	const pageResize =  (event) => {
-		contentHeight.value = event.target.innerHeight
-		console.log(contentHeight.value)
-		currentHeight.value = event.target.innerHeight < 720
-	}
+	const currentHeight = ref(null);
+
+	onMounted(() => {
+		currentHeight.value = container.value.getBoundingClientRect().height < 720
+		window.addEventListener("resize", (e) => currentHeight.value = e.target.innerHeight < 720)
+		sideList.value.addEventListener('scroll', () => {
+			let position = item.value.getBoundingClientRect().top;
+			console.log(position)
+			status.firstPosition = position > 100
+			status.secondPostion = position < 0
+		})
+	});
+
 
 </script>
