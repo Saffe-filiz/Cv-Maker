036e2bc1b8706c8d05cf35a7949735d83e8dc9e9
Saffe-filiz <saffetfiliz1989@gmail.com>
2023-07-13 09:14:55 +0300

    Next And Prevus button

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -6,9 +6,9 @@
 		<div class="w-full bg-white  p-4 xl:p-8 border-t border-borderColor">
 			<div class="w-full  flex justify-between">
 				<button class="h-[38px] px-6 text-[#4372ff] bg-white border border-[#4372ff] rounded"
-				:class="{'invisible' : currentSection == 0}" :disabled="currentSection == 0" >GO BACK</button>
+				:class="{'invisible' : currentSection == 0}" :disabled="currentSection == 0" @click="goToSection(-1)">GO BACK</button>
 				<button class="h-[38px] px-6 text-white bg-[#4372ff] border border-[#4372ff] rounded shadow-[0px_4px_7px_0px_rgba(67,114,255,0.3)]"
-				:class="{'invisible' : currentSection >= 6}" :disabled="currentSection < 6">NEXT</button>
+				:class="{'invisible' : currentSection == 6}" :disabled="currentSection == 6" @click="goToSection(+1)">NEXT</button>
 			</div>
 		</div>
 		<BottonBar/>
@@ -20,13 +20,18 @@
 	import MobileMenu from './TheMobileMenu.vue';
 
 	import { useStore } from 'vuex';
-	import { computed } from 'vue';
+	import { computed, ref } from 'vue';
 
 	const store = useStore();
 	const openMobileMenu = computed(() => store.getters.getMobileMenu);
-
+    
+    let number = ref(0)
 	const currentSection = computed(() => store.getters.getCurrentSection);
-	const goToSection = ( val ) => store.commit('changeCurrentSection', val);
+	const goToSection = (val) => {
+		number.value += val
+		console.log(number.value)
+	    store.commit('changeCurrentSection', number.value)
+	};
 
-	
+
 </script>
